02dacfd21263849a50231625e0634b339d77f2e4
Lisa Chen <lisa.chen23@imperial.ac.uk>
2023-10-18 19:19:11 +0100

    project commit

--- api/app.py
+++ api/app.py
@@ -2,12 +2,23 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def login_page():
     return render_template("index.html")
 
-
-@app.route("/submit", methods=["POST"])
-def submit():
+@app.route("/shop", methods=["POST"])
+def shop_page():
     input_name = request.form.get("name")
-    input_age = request.form.get("age")
-    return render_template("hello.html", name=input_name, age=input_age)
+    input_color = request.form.get("color")
+
+    return render_template("main_shop.html", name=input_name, color=input_color)
+
+@app.route("/checkout", methods=["POST"])
+def checkout():
+    
+    input_name = request.form.get("name")
+    input_color = request.form.get("color")
+
+    input_item = request.form.get("item")
+    input_quantity = request.form.get('quantity')
+
+    return render_template("checkout.html", name=input_name, color=input_color, item=input_item, quantity=input_quantity)
